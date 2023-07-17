a168c6e14230655126dfb8ac4fd903f191efa54b
Luzivog <thpfai@gmail.com>
2023-07-17 01:08:13 -0400

    no loading when scrolling & low loading time

--- App.tsx
+++ App.tsx
@@ -1,14 +1,19 @@
 
 import React from 'react';
 import MainContainer from './navigation/MainContainer';
-import { View } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { Dimensions, ScrollView } from 'react-native';
+import WebView from 'react-native-webview';
 
+
+const images = ["https://www.toonix.xyz/cdn_mangaraw/e-of-the-iron-bl/chapter-34/1.jpg", "https://www.toonix.xyz/cdn_mangaraw/e-of-the-iron-bl/chapter-34/2.jpg", "https://www.toonix.xyz/cdn_mangaraw/e-of-the-iron-bl/chapter-34/3.jpg", "https://www.toonix.xyz/cdn_mangaraw/e-of-the-iron-bl/chapter-34/4.jpg", "https://www.toonix.xyz/cdn_mangaraw/e-of-the-iron-bl/chapter-34/5.jpg", "https://www.toonix.xyz/cdn_mangaraw/e-of-the-iron-bl/chapter-34/6.jpg", "https://www.toonix.xyz/cdn_mangaraw/e-of-the-iron-bl/chapter-34/7.jpg", "https://www.toonix.xyz/cdn_mangaraw/e-of-the-iron-bl/chapter-34/8.jpg", "https://www.toonix.xyz/cdn_mangaraw/e-of-the-iron-bl/chapter-34/9.jpg", "https://www.toonix.xyz/cdn_mangaraw/e-of-the-iron-bl/chapter-34/10.jpg", "https://www.toonix.xyz/cdn_mangaraw/e-of-the-iron-bl/chapter-34/11.jpg", "https://www.toonix.xyz/cdn_mangaraw/e-of-the-iron-bl/chapter-34/12.jpg", "https://www.toonix.xyz/cdn_mangaraw/e-of-the-iron-bl/chapter-34/13.jpg", "https://www.toonix.xyz/cdn_mangaraw/e-of-the-iron-bl/chapter-34/14.jpg"]
+
+let html = `<html><head><meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" /></head>`
+html += "<body style='margin: 0 !important;padding: 0 !important;'>"
+for (let im of images) html+="<img style='width: 100%' src='"+im+"'>"
+html+="</body></html>"
 
 export default function App(): JSX.Element {
-
-
-  return (
-    <MainContainer/>
-  );
+	return (
+		<MainContainer/>
+	);
 };
--- navigation/Screens/ChapterScreen.tsx
+++ navigation/Screens/ChapterScreen.tsx
@@ -1,9 +1,12 @@
-import { FlatList, Image, Dimensions, StatusBar, View} from "react-native";
+import { FlatList, Image, Dimensions, StatusBar, View, ScrollView} from "react-native";
 import { ChapterScreenNavigationProp, ChapterScreenRouteProp } from "../stacks/WebtoonStack";
 import { useState, useEffect } from "react";
-import FastImage from 'react-native-fast-image'
+import FastImage from 'react-native-fast-image';
+import WebView from 'react-native-webview';
 import { fetchChapterImageUrls } from "../utils";
 import LoadingScreen from "./LoadingScreen";
+import AutoHeightWebView from 'react-native-autoheight-webview'
+
 
 const screenWidth = Dimensions.get('window').width;
 
@@ -14,21 +17,19 @@ export default function ChapterScreen({ navigation, route }: {
     const { webtoon, chapter } = route.params;
 
     const [isLoading, setIsLoading] = useState(true);
-    const [imageData, setImageData] = useState<Array<{uri: string, width: number, height: number}>>([]);
+    const [html, setHtml] = useState("");
 
     useEffect(() => {
 		(async () => {
             const urls = await fetchChapterImageUrls(webtoon, chapter);
-            setIsLoading(false);
 
-            for (const url of urls) {
-                await new Promise((resolve, reject) => {
-                    Image.getSize(url, (width, height) => {
-                        setImageData((prevData) => [...prevData, {uri: url, width, height}]);
-                        resolve(null);
-                    }, reject);
-                });
-            }
+            let localHtml = `<html><head><meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" /></head>`
+            localHtml += "<body style='margin: 0 !important;padding: 0 !important;'>";
+            for (let url of urls) localHtml+="<img style='width: 100%' src='"+url+"'>"
+            localHtml += "</body></html>";
+
+            setHtml(localHtml);
+            setIsLoading(false);
 		})();
 	}, [webtoon, chapter]);
 
@@ -37,21 +38,12 @@ export default function ChapterScreen({ navigation, route }: {
     return (
         <View>
             <StatusBar hidden/>
-            <FlatList
-                showsVerticalScrollIndicator= {false}
-                data={imageData}
-                renderItem={({ item }) => {
-                    const scaleFactor = screenWidth / item.width;
-                    const imageHeight = item.height * scaleFactor;
-                    return (
-                        <FastImage 
-                            source={{ uri: item.uri }} 
-                            style={{ width: screenWidth, height: imageHeight }} 
-                        />
-                    );
-                }}
-                keyExtractor={(item, index) => index.toString()}
-            />
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <AutoHeightWebView
+                    source={{html:html}}
+                    style={{width: Dimensions.get('window').width}}
+                />
+            </ScrollView>
         </View>
     );
 }
