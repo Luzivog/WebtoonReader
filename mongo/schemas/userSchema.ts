4e76a4a9c659746bfb0b845d5427f042d33fa71f
Luzivog <thpfai@gmail.com>
2023-07-22 18:09:50 -0400

    User schema created

---
+++ mongo/schemas/userSchema.ts
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const {Schema, model} = mongoose;
+
+const userSchema = new Schema({
+    username: String,
+    password: String,
+    bookmarks: [String],
+    viewed: {
+        type: Map,
+        of: [String]
+    },
+});
+
+const User = model("User", userSchema);
+
+export default User;
--- tests/test.js
+++ tests/test.js
@@ -1,10 +1,46 @@
 const mongoose = require('mongoose');
-require("dotenv").config();
+const {Schema, model} = mongoose;
+require('dotenv').config();
+
+const userSchema = new Schema({
+    username: String,
+    password: String,
+    bookmarks: [String],
+    viewed: {
+        type: Map,
+        of: [String]
+    },
+});
+
+const User = model("User", userSchema);
+
 
 (async() => {
     try {
         await mongoose.connect(process.env.MONGO_URL);
         console.log("Connected to MongoDB");
+
+        const user = new User({
+            username: 'testUser',
+            password: 'testPassword',
+            bookmarks: [],
+            viewed: new Map()
+        });
+        user.save()
+            .then((doc) => {
+                console.log("Saved user", doc);
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+
+        User.findOne({ username: 'testUser' })
+            .then((doc) => {
+                console.log("User found", doc);
+            })
+            .catch((err) => {
+                console.error(err);
+            });
     } catch (error) {
         console.log(error);
     }
