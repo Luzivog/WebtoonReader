a2f4896049393d04478cb010c0ba48d5ca14ccbe
Luzivog <thpfai@gmail.com>
2023-07-25 11:12:57 -0400

    create a script to start the app & api

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Open new terminal tab and run 'npm run start-api'
+gnome-terminal --tab -- bash -c "npm run start-api; exec bash"
+
+# Get the window ID of the current terminal
+window_id=$(xdotool getactivewindow)
+xdotool windowactivate --sync $window_id key Ctrl+Page_Up
+
+# Run 'npm run start-react' in current terminal
+npm run start-react
