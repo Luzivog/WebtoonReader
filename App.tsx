2744b4d09635adfc37ca3d14f247a1395246eb2b
Luzivog <thpfai@gmail.com>
2023-09-07 10:08:11 -0400

    login & register screen start

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
 
 import React from 'react';
 import MainContainer from './navigation/MainContainer';
+import LoginScreen from './navigation/Screens/LoginScreen';
 
 export default function App(): JSX.Element {
 	return (
-		<MainContainer/>
+		<LoginScreen/>
 	);
 };
---
+++ navigation/Screens/LoginScreen.tsx
@@ -0,0 +1,151 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image } from 'react-native';
+import CheckBox from '@react-native-community/checkbox';
+import Icon from 'react-native-vector-icons/FontAwesome'; // Import the icon library
+
+function LoginScreen({ navigation }): JSX.Element {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [isChecked, setIsChecked] = useState(false);
+    const [isPasswordVisible, setIsPasswordVisible] = useState(false); // Track password visibility
+
+    const handleLogin = () => {
+        // Implement form validation and sign-up logic here
+    };
+
+    const handleRegister = () => {
+        // Navigate to the sign-in screen using the navigation prop
+        //navigation.navigate('Register');
+    };
+
+    const togglePasswordVisibility = () => {
+        setIsPasswordVisible(!isPasswordVisible);
+    };
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Login</Text>
+            <TextInput
+                placeholder="Username"
+                style={styles.input}
+                value={username}
+                onChangeText={(text) => setUsername(text)}
+            />
+            <View style={styles.passwordContainer}>
+                <TextInput
+                    placeholder="Password"
+                    secureTextEntry={!isPasswordVisible} // Toggle secureTextEntry based on visibility
+                    style={styles.passwordInput}
+                    value={password}
+                    onChangeText={(text) => setPassword(text)}
+                />
+                <TouchableOpacity
+                    style={styles.eyeIcon}
+                    onPress={togglePasswordVisibility} // Toggle password visibility on press
+                >
+                    <Icon
+                        name={isPasswordVisible ? 'eye' : 'eye-slash'}
+                        size={20}
+                        color="#333"
+                    />
+                </TouchableOpacity>
+            </View>
+
+            <View style={styles.termsContainer}>
+                <CheckBox
+                    value={isChecked}
+                    onValueChange={(value) => setIsChecked(value)}
+                />
+                <Text style={styles.termsText}>
+                    I agree to the <Text style={styles.linkText}>Terms and Conditions</Text>
+                </Text>
+            </View>
+
+            <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
+                <Text style={styles.loginText}>Login</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.registerButton} onPress={handleRegister}>
+                <Text style={styles.registerText}>Register</Text>
+            </TouchableOpacity>
+
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#252525',
+    },
+    title: {
+        fontSize: 24,
+        color: '#fff',
+        fontWeight: 'bold',
+        marginBottom: 20,
+    },
+    input: {
+        backgroundColor: '#fff',
+        width: '80%',
+        padding: 10,
+        marginBottom: 10,
+        borderRadius: 5,
+    },
+    passwordContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        width: '80%',
+        marginBottom: 10,
+        borderRadius: 5,
+        backgroundColor: '#fff',
+    },
+    passwordInput: {
+        flex: 1,
+        padding: 10,
+    },
+    eyeIcon: {
+        padding: 10,
+    },
+    loginButton: {
+        backgroundColor: 'tomato',
+        width: '80%',
+        padding: 15,
+        borderRadius: 5,
+        alignItems: 'center',
+        marginTop: 25,
+    },
+    loginText: {
+        color: '#fff',
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+    registerButton: {
+        backgroundColor: '#555555',
+        width: '80%',
+        padding: 15,
+        borderRadius: 5,
+        alignItems: 'center',
+        marginTop: 10,
+    },
+    registerText: {
+        color: '#fff',
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+    termsContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    termsText: {
+        fontSize: 14,
+        color: '#fff',
+    },
+    linkText: {
+        color: 'blue',
+        textDecorationLine: 'underline',
+    },
+});
+
+export default LoginScreen;
