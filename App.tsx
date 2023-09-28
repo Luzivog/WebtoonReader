f1e03e894a234a69d14cdaa26e3d2d1072d5127a
Luzivog <thpfai@gmail.com>
2023-09-27 23:22:52 -0400

    env vars

--- App.tsx
+++ App.tsx
@@ -1,10 +1,5 @@
 import React from 'react';
 import MainContainer from './navigation/MainContainer';
-import LoginScreen from './navigation/Screens/LoginScreen';
-
-
-require('dotenv').config();
-console.log(process.env);
 
 export default function App(): JSX.Element {
 	return (
